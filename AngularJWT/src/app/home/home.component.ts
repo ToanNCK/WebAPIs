a018816f9c1ed821519c937cb29a4dd866639149
toan nguyen <toannck32@wru.vn>
2021-07-28 17:19:47 +0700

    update angular

---
+++ AngularJWT/src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { UserService } from '@app/_services';
+import $ from "jquery";
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    loading = false;
+    users: User[];
+
+
+    constructor(private userService: UserService) { }
+
+    ngOnInit() {
+        this.loading = true;
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.loading = false;
+            this.users = users;
+        });
+
+        $('.is-img').on('click', function (e) {
+            let parentOffset = $(this).offset(); 
+            let cloneBtn = $('.btn-info-img').clone()
+            $(this).append(cloneBtn.css({ 'top': (e.pageY - parentOffset.top - 16) + 'px', 'left': (e.pageX - parentOffset.left - 16) + 'px' }))
+        })
+    }
+}
